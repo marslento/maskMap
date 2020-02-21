bfcf1d7fdbca8f27b898069a4f1d7a3ea40a94fe
Marcia Wu <marciawu@fareastone.com.tw>
2020-02-21 16:04:20 +0800

    deploy

---
+++ precache-manifest.5e89216b5fc758914518159f12a35f94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c63c31f264f72aa4e7",
+    "url": "/maskMap/css/chunk-vendors.774964f5.css"
+  },
+  {
+    "revision": "383de32690c0a5203083",
+    "url": "/maskMap/css/index.b9bf5d58.css"
+  },
+  {
+    "revision": "ef7bfb9c7c093503a4139aa0641d1de3",
+    "url": "/maskMap/index.html"
+  },
+  {
+    "revision": "e1c63c31f264f72aa4e7",
+    "url": "/maskMap/js/chunk-vendors.f86fc3d8.js"
+  },
+  {
+    "revision": "383de32690c0a5203083",
+    "url": "/maskMap/js/index.02fc1b6d.js"
+  },
+  {
+    "revision": "4d5fdd69c077be42069c805b54b7e613",
+    "url": "/maskMap/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/maskMap/robots.txt"
+  }
+]);
